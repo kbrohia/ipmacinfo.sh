ef627a801706874aeb761a20497c05da2847ba4f
kbrohia <kyle.brody@hopeisalive.net>
2025-04-23 09:28:50 -0500

    Update ipmacinfo.sh
    added many other diagnostic categories

--- ipmacinfo.sh
+++ ipmacinfo.sh
@@ -1,28 +1,33 @@
-echo "=============Interface IPs:================"
-if command -v ifconfig &> /dev/null
-then
-    echo "Interface IPs:"
-    ifconfig | grep 'inet ' | awk '{print $2}'
-elif command -v ip &> /dev/null
-then
-    echo "Interface IPs:"
-    ip -4 addr show | grep inet | awk '{print $2}'
-fi
+#!/bin/bash
 
-echo " "
-echo " "
-echo "=============Wi-Fi MAC Addresses================"
-# Find the Wi-Fi interface (usually en0 or en1)
-wifi_interface=$(networksetup -listallhardwareports | \
-    awk '/Wi-Fi|AirPort/{getline; print $2}')
+# Get Wi-Fi interface
+wifi_interface=$(networksetup -listallhardwareports | awk '/Wi-Fi|AirPort/{getline; print $2}')
 
-# Get the MAC address of the Wi-Fi interface
 if [ -n "$wifi_interface" ]; then
     mac_address=$(ifconfig $wifi_interface | awk '/ether/{print $2}')
+    ip_address=$(ipconfig getifaddr $wifi_interface)
+    ipv6_address=$(ifconfig $wifi_interface | awk '/inet6 / && !/fe80/ {print $2}')
+    ssid=$(networksetup -getairportnetwork $wifi_interface | awk -F': ' '{print $2}')
+    bssid=$(airport -I | awk '/BSSID/ {print $2}')
+    rssi=$(airport -I | awk '/agrCtlRSSI/ {print $2}')
+    noise=$(airport -I | awk '/agrCtlNoise/ {print $2}')
+    gateway=$(netstat -rn | grep default | awk '{print $2}')
+    dns_servers=$(networksetup -getdnsservers $wifi_interface | grep -v "There aren't any")
+    public_ip=$(curl -s https://api.ipify.org)
+    
+    echo "===== Network Information ====="
     echo "Wi-Fi Interface: $wifi_interface"
     echo "MAC Address: $mac_address"
+    echo "IPv4 Address: $ip_address"
+    echo "IPv6 Address: $ipv6_address"
+    echo "SSID: $ssid"
+    echo "BSSID: $bssid"
+    echo "Signal Strength (RSSI): $rssi dBm"
+    echo "Noise Level: $noise dBm"
+    echo "Default Gateway: $gateway"
+    echo "DNS Servers:"
+    echo "$dns_servers"
+    echo "Public IP: $public_ip"
 else
     echo "Wi-Fi interface not found."
 fi
-echo " "
-echo "Nice one. Very cool."
