1a15c5d89d866d542e55270e4a38e4ca3e1e45db
kbrohia <kyle.brody@hopeisalive.net>
2025-04-23 11:41:50 -0500

    Update ipmacinfo.sh
    updated script to use networksetup and ipconfig getsummary since airport is deprecated/to simplify code. removed rssi.

--- ipmacinfo.sh
+++ ipmacinfo.sh
@@ -1,20 +1,17 @@
-#!/bin/bash
-
 # Get Wi-Fi interface
 wifi_interface=$(networksetup -listallhardwareports | awk '/Wi-Fi|AirPort/{getline; print $2}')
+wifi_summary=$(/usr/sbin/ipconfig getsummary "$wifi_interface")
 
 if [ -n "$wifi_interface" ]; then
     mac_address=$(ifconfig $wifi_interface | awk '/ether/{print $2}')
     ip_address=$(ipconfig getifaddr $wifi_interface)
-    ipv6_address=$(ifconfig $wifi_interface | awk '/inet6 / && !/fe80/ {print $2}')
-    ssid=$(networksetup -getairportnetwork $wifi_interface | awk -F': ' '{print $2}')
-    bssid=$(airport -I | awk '/BSSID/ {print $2}')
-    rssi=$(airport -I | awk '/agrCtlRSSI/ {print $2}')
-    noise=$(airport -I | awk '/agrCtlNoise/ {print $2}')
+    ipv6_address=$(ifconfig $wifi_interface | awk '/inet6 / && !/fe80/ {print $2}' | head -n 1)
+    ssid=$(ipconfig getsummary $(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/{getline; print $2}') | awk -F ' SSID : ' '/ SSID : / {print $2}')
+    bssid=$(echo "$wifi_summary" | awk -F 'BSSID : ' '/BSSID/ {print $2}')
     gateway=$(netstat -rn | grep default | awk '{print $2}')
     dns_servers=$(networksetup -getdnsservers $wifi_interface | grep -v "There aren't any")
     public_ip=$(curl -s https://api.ipify.org)
-    
+
     echo "===== Network Information ====="
     echo "Wi-Fi Interface: $wifi_interface"
     echo "MAC Address: $mac_address"
@@ -25,8 +22,7 @@ if [ -n "$wifi_interface" ]; then
     echo "Signal Strength (RSSI): $rssi dBm"
     echo "Noise Level: $noise dBm"
     echo "Default Gateway: $gateway"
-    echo "DNS Servers:"
-    echo "$dns_servers"
+    echo "DNS Servers: $dns_servers"
     echo "Public IP: $public_ip"
 else
     echo "Wi-Fi interface not found."
